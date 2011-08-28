e731dc81ecb0b9aca0714f9aeae549e2200e36ec
John D'Alessio <john@daless.com>
2011-08-27 23:50:47 -0700

    initial commit of daless.com site

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DalessCom::Application.config.secret_token = '481f73a94f3b7733825fa3a8156f6cc1549d932bd7f683f907b4862193cf3730ebe0e46eabd860dcd32f5418c0f955378d420cfd879f77c30cf6a6bb8ec3eca8'
